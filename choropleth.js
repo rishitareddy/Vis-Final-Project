efe66857f5c095b1c6802cee77cc6da243615b36
Deepika-Manchikanti <41280540+Deepika-Manchikanti@users.noreply.github.com>
2021-05-12 09:22:07 +0530

    JS for choropleth

---
+++ choropleth.js
@@ -0,0 +1,109 @@
+var width = 400;
+    height = 300;
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-5, 0])
+  .html(function(d) {
+    var dataRow = countryById.get(d.properties.name);
+       if (dataRow) {
+           console.log(dataRow);
+           return dataRow.state + ": " + dataRow.value;
+       } else {
+           console.log("no dataRow", d);
+           return d.properties.name + ": No data.";
+       }
+  })
+
+
+var svg1 = d3.select('#vis').append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+svg1.call(tip);
+
+var projection = d3.geoAlbersUsa()
+    .scale(500) // mess with this if you want
+    .translate([width / 2, height / 2]);
+
+var path = d3.geoPath()
+    .projection(projection);
+
+var colorScale = d3.scaleLinear().range(["#D4EEFF", "#0099FF"]).interpolate(d3.interpolateLab);
+
+var countryById = d3.map();
+
+// we use queue because we have 2 data files to load.
+queue()
+    .defer(d3.json, "us-states.json")
+    .defer(d3.csv, "statesdata.csv", typeAndSet) // process
+    .await(loaded);
+
+function typeAndSet(d) {
+    d.value = +d.value;
+    countryById.set(d.state, d);
+    return d;
+}
+
+function getColor(d) {
+    var dataRow = countryById.get(d.properties.name);
+    if (dataRow) {
+        console.log(dataRow);
+        return colorScale(dataRow.value);
+    } else {
+        console.log("no dataRow", d);
+        return "#ccc";
+    }
+}
+
+
+function loaded(error, usa, value) {
+
+    console.log(usa);
+    console.log(value);
+
+    colorScale.domain(d3.extent(value, function(d) {return d.value;}));
+
+    var states = topojson.feature(usa, usa.objects.units).features;
+
+    svg1.selectAll('path.state')
+        .data(states)
+        .enter()
+        .append('path')
+        .attr('class', 'states')
+        .attr('d', path)
+		.on('mouseover',function(d){
+			tip.show(d);
+			d3.select(this)
+			  .style('opacity', 1)
+			  .style('stroke-width', 3);
+		  })
+		  .on('mouseout', function(d){
+			tip.hide(d);
+			d3.select(this)
+			  .style('opacity', 0.8)
+			  .style('stroke-width',0.3);
+		  })
+        .attr('fill', function(d,i) {
+            console.log(d.properties.name);
+            return getColor(d);
+        })
+        .append("title");
+
+    var linear = colorScale;
+
+    svg1.append("g")
+      .attr("class", "legendLinear")
+      .attr("transform", "translate(-5,-5)");
+
+    var legendLinear = d3.legendColor()
+      .shapeWidth(30)
+      .orient('horizontal')
+      .scale(linear);
+
+    svg1.select(".legendLinear")
+      .call(legendLinear);
+
+}
+
+
